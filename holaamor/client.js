69da6a97cf8bb009f5f96232dc2db0ebc13de24a
Vrokn <61293690+Vrokn@users.noreply.github.com>
2020-12-02 20:00:49 -0500

    Updated app

--- holaamor/client.js
+++ holaamor/client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance, Surface } from "react-360-web";
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -9,14 +9,18 @@ function init(bundle, parent, options = {}) {
     fullScreen: true,
     ...options,
   });
-  
+
   r360.renderToSurface(
-    r360.createRoot('lovetest', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot("lovetest", {
+      /* initial props */
+    }),
+    new Surface(4680, 1200, Surface.SurfaceShape.Cylinder)
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('30026348064_58410ce1fe_o.jpg'));
+  r360.compositor.setBackground(
+    r360.getAssetURL("30026348064_58410ce1fe_o.jpg")
+  );
 }
 
-window.React360 = {init};
+window.React360 = { init };
--- holaamor/index.js
+++ holaamor/index.js
@@ -12,25 +12,10 @@ import {
 import sonidos from "./components/Sonidos";
 const { AudioModule } = NativeModules;
 export default class lovetest extends React.Component {
-  /*  // Our component will keep track of this state
-  state = {
-    count: 0,
-  };
-
-  // This method increments our count, triggering a re-render
-  _incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  // Once the component mounts, run the increment method every second
-  componentDidMount() {
-    console.log(sonidos);
-    setInterval(this._incrementCount, 1000);
-  } */
-
+  
   render() {
     return (
-      <View style={{ transform: [{ translate: [0, 0, -2] }], flex:1 }}>
+      <View>
         {sonidos.map((item, index) => (
           <VrButton
             onClick={() => {
@@ -42,22 +27,19 @@ export default class lovetest extends React.Component {
             style={{
               justifyContent: "center",
               alignItems: "center",
-              width: 100,
-              height: 100,
-              borderRadius: 50,
-              shadowColor: "#d9534f",
-              shadowOffset: { width: 30, height: 10 },
-              shadowOpacity: 1,
-              shadowRadius: 0,
+              width: 150,
+              height: 150,
+              borderRadius: 75,
               padding: 0,
               backgroundColor: item.color,
-              borderColor: "transparent",
+              borderColor: "#373854",
               borderWidth: 4,
-              transform: [{ translate: [index * 100, index*50, 0] }],
+              position: "absolute",
+              left: Math.floor(Math.random() * 4400) + 150,
+              top: Math.floor(Math.random() * 1000) + 150,
             }}
           >
-            <Text style={styles.red}>{item.name}</Text>
-          </VrButton>
+         </VrButton>
         ))}
       </View>
     );
@@ -70,27 +52,12 @@ const styles = StyleSheet.create({
   },
   panel: {
     // Fill the entire surface
-    width: 2000,
+    width: 4000,
     height: 600,
     backgroundColor: "rgba(255, 255, 255, 0.5)",
     justifyContent: "center",
     alignItems: "center",
   },
-  greetingBox: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    padding: 0,
-    backgroundColor: "#000000",
-    borderColor: "#639dda",
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
 });
 
 AppRegistry.registerComponent("lovetest", () => lovetest);
-
